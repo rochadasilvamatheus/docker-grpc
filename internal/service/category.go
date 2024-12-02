229071a85f965e4e0b8823474951c9b065b695ed
rochadasilvamatheus <mathewsr.silva@gmail.com>
2024-12-02 17:01:36 -0300

    feat: criação de categoria por stream

--- internal/service/category.go
+++ internal/service/category.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"io"
 	"context"
 	"gRPC/internal/pb"
 	"gRPC/internal/database"
@@ -51,4 +52,68 @@ func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb
 	}
 
 	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
+
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Find(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id: category.ID,
+		Name: category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id: categoryResult.ID,
+			Name: categoryResult.Name,
+			Description: category.Description,
+		})
+	}
+}
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		err = stream.Send(&pb.Category{
+			Id: categoryResult.ID,
+			Name: categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		if err != nil {
+			return err
+		}
+	}
 }
